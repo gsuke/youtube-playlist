c6269114a8735c520964d401bb8f2480a651680a
gsuke <54441258+gsuke@users.noreply.github.com>
2023-12-05 17:44:24 +0900

    feat: get playlist items

--- get_playlist_items.go
+++ get_playlist_items.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
 
-	getClient(youtube.YoutubeReadonlyScope)
+	const playlistId = ""
+
+	client := getClient(youtube.YoutubeReadonlyScope)
+
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Error creating YouTube client: %v", err)
+	}
+
+	// Make the API call to YouTube.
+	response, err := service.PlaylistItems.List([]string{"snippet,status"}).
+		PlaylistId(playlistId).
+		MaxResults(3).
+		Do()
+	handleError(err, "")
+
+	for _, item := range response.Items {
+		fmt.Printf(
+			"動画: %v(%v), チャンネル: %v(%v), %v\n",
+			item.Snippet.Title,
+			item.Snippet.ResourceId.VideoId,
+			item.Snippet.VideoOwnerChannelTitle,
+			item.Snippet.VideoOwnerChannelId,
+			item.Status.PrivacyStatus,
+		)
+	}
 
 }
